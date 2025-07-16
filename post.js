b5fe2b9f6750e4044090ac6ba9df02ee60280b0c
PannocchiaNicola <pannocchianicola@gmail.com>
2025-07-16 15:21:51 +0200

    Add post creation backend

---
+++ post.js
@@ -0,0 +1,10 @@
+class Post {
+constructor(content, author) {
+this.content = content;
+this.author = author; 
+this.timestamp = new Date();
+}
+}
+function createPost(content, author) {
+return new Post(content, author);
+}
